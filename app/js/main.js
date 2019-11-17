2071f004636ec0fe18429bd7b5349da635d78ace
Sofia Varich <kirisona@gmail.com>
2019-11-17 16:31:13 +0200

    slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,37 +1,18 @@
-let menuToggle = document.querySelector('.menu');
-let menu = document.querySelector('.header__list');
-
-menuToggle.addEventListener('click', function(e) {
-    menu.classList.toggle('active');
+$(".menu").on("click", function() {
+  $(".header__list").toggleClass("active");
 });
 
 $(".slider").slick({
-
-    // normal options...
-    slidesToShow: 1,
-  slidesToScroll: 1,
-  
-    // the magic
-    responsive: [{
-  
-        breakpoint: 991,
-        settings: {
-          slidesToShow: 1,
-          infinite: true
-        }
-  
-      }, {
-  
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          dots: true
-        }
-  
-      }, {
-  
-        breakpoint: 300,
-        settings: "unslick" // destroys slick
-  
-      }]
-  });
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true
+      }
+    }
+  ]
+});
